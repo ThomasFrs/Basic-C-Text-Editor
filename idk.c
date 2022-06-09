b4ea5eb1c2158ad2abc8f38574ce0f1f3176e199
thomasfrs <thomasfrs44@gmail.com>
2022-06-09 22:25:20 +0200

    Added basic highlighting

---
+++ idk.c
@@ -0,0 +1,2 @@
+test  sdf what the hell  sad 11241243 asfsf  while  
+test if  while  nwhilewhilewhile   while   while     while   whilewhile while while                                                                  
